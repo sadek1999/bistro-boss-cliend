f26e004a3fd04d38c9fa96fd68e8546558f2ddbf
Sadek Ali <sadekali1530@gmail.com>
2024-01-12 19:20:24 +0600

    set footer as desine

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
+import Navbar from '../shear/navbar/Navbar';
+import Footer from '../shear/footer/Footer';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,9 @@ import { router } from './Routs/Routs';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-7xl mx-auto bg-white'>
     <RouterProvider router={router} />
+    </div>
+    
   </React.StrictMode>,
 )
