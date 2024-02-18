851507d0ea0a04f2a130307b657f2830965efb45
sadek1999 <sadekali1530@gmail.com>
2024-02-18 11:54:14 +0600

    new

--- src/pages/Home/banner/Banner.jsx
+++ src/pages/Home/banner/Banner.jsx
@@ -6,6 +6,8 @@ const Banner = () => {
         <div>
          <h1>This is banner page for this code </h1>   
          <h1>This is banner page for this code </h1>   
+         <h1>This is banner page for this code </h1>   
+         <h1>This is banner page for this code </h1>   
         </div>
     );
 };
